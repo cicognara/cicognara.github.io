de5f10256c307d1a6320ea1c08a268eb9933fca6
Axa Mei Liauw <aliauw@princeton.edu>
2016-03-02 13:10:01 -0500

    Update image captions and contact form

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,10 @@
 $(function() {
-  $('body').addClass('loaded');
+    $('body').addClass('loaded');
 });
 
-$(document).scroll(function(){
-     $('.intro').toggleClass('scrolled', $(this).scrollTop() > 1);
- });
+$(document).scroll(function() {
+    $('.intro').toggleClass('scrolled', $(this).scrollTop() > 1);
+});
 
 // Hide Header on on scroll down
 var didScroll;
@@ -12,7 +12,7 @@ var lastScrollTop = 0;
 var delta = 5;
 var navbarHeight = $('header').outerHeight();
 
-$(window).scroll(function(event){
+$(window).scroll(function(event) {
     didScroll = true;
 });
 
@@ -26,18 +26,41 @@ setInterval(function() {
 function hasScrolled() {
     var st = $(this).scrollTop();
 
-    if(Math.abs(lastScrollTop - st) <= delta)
+    if (Math.abs(lastScrollTop - st) <= delta)
         return;
 
-    if (st > lastScrollTop && st > navbarHeight){
+    if (st > lastScrollTop && st > navbarHeight) {
         // Scroll Down
         $('header').removeClass('nav-down').addClass('nav-up');
     } else {
         // Scroll Up
-        if(st + $(window).height() < $(document).height()) {
+        if (st + $(window).height() < $(document).height()) {
             $('header').removeClass('nav-up').addClass('nav-down');
         }
     }
 
     lastScrollTop = st;
 }
+
+// contact form
+var $contactForm = $("#contact-form");
+$contactForm.submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+        url: '//formspree.io/axa.liauw@gmail.com',
+        method: 'POST',
+        data: $(this).serialize(),
+        dataType: 'json',
+        beforeSend: function() {
+            $contactForm.append('<div class="alert alert--loading"><p>Sending message…</p></div>');
+        },
+        success: function(data) {
+            $contactForm.find('.alert--loading').hide();
+            $contactForm.append('<div class="alert alert--success"><p>Message sent!</p></div>');
+        },
+        error: function(err) {
+            $contactForm.find('.alert--loading').hide();
+            $contactForm.append('<div class="alert alert--error"><p>Oops, there was an error.</p></div>');
+        }
+    });
+});
